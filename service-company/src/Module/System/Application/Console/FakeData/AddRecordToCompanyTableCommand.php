0987fb1a7588b8740e1667909e5da9de3881c85f
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2025-06-10 21:01:24 +0200

    Add command for companies fake data

--- service-company/src/Module/System/Application/Console/FakeData/AddRecordToCompanyTableCommand.php
+++ service-company/src/Module/System/Application/Console/FakeData/AddRecordToCompanyTableCommand.php
@@ -41,55 +41,57 @@ class AddRecordToCompanyTableCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $data = $this->companyFakeData->getDefaultData();
+        $items = $this->companyFakeData->getFakeData();
+        foreach ($items as $data) {
+            $companyRepository = $this->entityManager->getRepository(Company::class);
+            $existingCompany = $companyRepository->createQueryBuilder(Company::ALIAS)
+                ->where(Company::ALIAS . '.name = :name')
+                ->orWhere(Company::ALIAS . '.nip = :nip')
+                ->setParameters(new ArrayCollection([
+                    new Parameter('name', $data['name']),
+                    new Parameter('nip', $data['nip']),
+                ]))
+                ->getQuery()
+                ->getOneOrNullResult();
 
-        $companyRepository = $this->entityManager->getRepository(Company::class);
-        $existingCompany = $companyRepository->createQueryBuilder(Company::ALIAS)
-            ->where(Company::ALIAS . '.name = :name')
-            ->orWhere(Company::ALIAS . '.nip = :nip')
-            ->setParameters(new ArrayCollection([
-                new Parameter('name', $data['name']),
-                new Parameter('nip', $data['nip']),
-            ]))
-            ->getQuery()
-            ->getOneOrNullResult();
+            if ($existingCompany !== null) {
+                $output->writeln(sprintf('<comment>%s: nazwa: %s lub NIP: %d</comment>', self::INFO_ALREADY_EXISTS, $data['name'], $data['nip']));
 
-        if ($existingCompany !== null) {
-            $output->writeln(sprintf('<comment>%s</comment>', self::INFO_ALREADY_EXISTS));
-            return Command::SUCCESS;
+                continue;
+            }
+
+            $company = new Company();
+            $company->setName($data['name']);
+            $company->setNip($data['nip']);
+
+            foreach ($data['phones'] as $phoneNumber) {
+                $contact = new Contact();
+                $contact->setType(ContactTypeEnum::PHONE->value);
+                $contact->setData($phoneNumber);
+                $contact->setCompany($company);
+                $this->entityManager->persist($contact);
+            }
+
+            foreach ($data['emails'] as $emailAddress) {
+                $contact = new Contact();
+                $contact->setType(ContactTypeEnum::EMAIL->value);
+                $contact->setData($emailAddress);
+                $contact->setCompany($company);
+                $this->entityManager->persist($contact);
+            }
+
+            $addressData = $data['address'];
+            $address = new Address();
+            $address->setStreet($addressData['street']);
+            $address->setPostcode($addressData['postcode']);
+            $address->setCity($addressData['city']);
+            $address->setCountry($addressData['country']);
+            $address->setCompany($company);
+
+            $this->entityManager->persist($address);
+            $this->entityManager->persist($company);
+            $this->entityManager->flush();
         }
-
-        $company = new Company();
-        $company->setName($data['name']);
-        $company->setNip($data['nip']);
-
-        foreach ($data['phones'] as $phoneNumber) {
-            $contact = new Contact();
-            $contact->setType(ContactTypeEnum::PHONE->value);
-            $contact->setData($phoneNumber);
-            $contact->setCompany($company);
-            $this->entityManager->persist($contact);
-        }
-
-        foreach ($data['emails'] as $emailAddress) {
-            $contact = new Contact();
-            $contact->setType(ContactTypeEnum::EMAIL->value);
-            $contact->setData($emailAddress);
-            $contact->setCompany($company);
-            $this->entityManager->persist($contact);
-        }
-
-        $addressData = $data['address'];
-        $address = new Address();
-        $address->setStreet($addressData['street']);
-        $address->setPostcode($addressData['postcode']);
-        $address->setCity($addressData['city']);
-        $address->setCountry($addressData['country']);
-        $address->setCompany($company);
-
-        $this->entityManager->persist($address);
-        $this->entityManager->persist($company);
-        $this->entityManager->flush();
 
         $output->writeln(sprintf('<info>%s</info>', self::SUCCESS_MESSAGE));
 
--- service-company/src/Module/System/Application/Console/FakeData/Data/Company.php
+++ service-company/src/Module/System/Application/Console/FakeData/Data/Company.php
@@ -7,27 +7,62 @@ namespace App\Module\System\Application\Console\FakeData\Data;
 final readonly class Company
 {
 
-    public function getDefaultData(): array
+    public function getFakeData(): array
     {
         return [
-            'name' => 'Feture Technology',
-            'nip' => '9316831327',
-            'phones' => [
-                '155-555-555',
-                '255-555-555',
-                '355-555-555',
+            [
+                'name'    => 'Future Technology',
+                'nip'     => '7965645023',
+                'phones'  => [
+                    '111-555-555',
+                    '112-555-555',
+                    '113-555-555',
+                ],
+                'emails'  => [
+                    'futuretechnnology_1@example.com',
+                    'futuretechnnology_2@example.com',
+                    'futuretechnnology_3@example.com',
+                ],
+                'address' => [
+                    'street'   => 'Wiejska 1',
+                    'postcode' => '11-111',
+                    'city'     => 'Gdańsk',
+                    'country'  => 'Polska',
+                ],
             ],
-            'emails' => [
-                'futuretechnnology_1@example.com',
-                'futuretechnnology_2@example.com',
-                'futuretechnnology_3@example.com',
+            [
+                'name'    => 'NCBR',
+                'nip'     => '9711228630',
+                'phones'  => [
+                    '221-555-555',
+                ],
+                'emails'  => [
+                    'ncbr_1@example.com',
+                    'ncbr_2@example.com',
+                ],
+                'address' => [
+                    'street'   => 'Polna 21/3c',
+                    'postcode' => '22-222',
+                    'city'     => 'Gdańsk',
+                    'country'  => 'Polska',
+                ],
             ],
-            'address' => [
-                'street' => 'Wiejska 1',
-                'postcode' => '11-111',
-                'city' => 'Gdańsk',
-                'country' => 'Polska',
-                'active' => true,
+            [
+                'name'    => 'CPK',
+                'nip'     => '7629966558',
+                'phones'  => [
+                    '331-555-555',
+                    '332-555-555'
+                ],
+                'emails'  => [
+                    'cpk_1@example.com',
+                ],
+                'address' => [
+                    'street'   => 'Niewidoczna 3',
+                    'postcode' => '33-333',
+                    'city'     => 'Warszawa',
+                    'country'  => 'Polska',
+                ],
             ],
         ];
     }
